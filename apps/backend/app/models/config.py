2e9e3db7cede4cbf0790a9071c24bbc2e791c097
Aagam Ratadia <143749760+Aagamratadia@users.noreply.github.com>
2026-01-29 13:51:25 +0530

    Update
    cap

--- apps/backend/app/models/config.py
+++ apps/backend/app/models/config.py
@@ -3,7 +3,7 @@ from pydantic import Field
 
 class AppConfig(Document):
     registration_open: bool = Field(default=True)
-    registration_cap: int = Field(default=174)
+    registration_cap: int = Field(default=200)
 
     class Settings:
         name = "app_config"
